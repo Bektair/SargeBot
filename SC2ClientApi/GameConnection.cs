35e63b6ebe88116312a711324a72696b186aca6d
Dagfinn Reitan <dagfinn.reitan@gyldendal.no>
2022-06-03 11:25:04 +0200

    Added response handlers

--- SC2ClientApi/GameConnection.cs
+++ SC2ClientApi/GameConnection.cs
@@ -10,9 +10,14 @@ internal class GameConnection
     private const int READ_BUFFER = 1024 * 1024;
     private const int MAX_CONNECTION_ATTEMPTS = 25;
     private const int TIMEOUT = 550; //ms
+    private readonly ResponseHandler _responseHandler;
     private readonly CancellationToken token = new CancellationTokenSource().Token;
-
     private ClientWebSocket? _socket;
+
+    public GameConnection()
+    {
+        _responseHandler = new();
+    }
 
     public Status Status { get; private set; }
 
@@ -41,21 +46,35 @@ internal class GameConnection
         if (_socket.State != WebSocketState.Open)
             return false;
 
+        Task.Run(ReceiveForever);
+
         var pingResponse = await SendAndReceiveAsync(ClientConstants.RequestPing);
         return pingResponse.Ping.HasGameVersion;
     }
 
     public async Task<Response> SendAndReceiveAsync(Request req)
     {
-        await SendAsync(req);
+        Response response = null;
 
-        var debugMe = await ReceiveAsync();
-        return debugMe;
+        var marker = new Task(() => { });
+        var handler = new Action<Response>(r =>
+        {
+            response = r;
+            marker.RunSynchronously();
+        });
+
+        _responseHandler.RegisterHandler(req.RequestCase, handler);
+        await SendAsync(req);
+        marker.Wait(TIMEOUT);
+        _responseHandler.DeregisterHandler(req.RequestCase);
+
+        return response;
     }
 
     public async Task SendAsync(Request req) => await _socket.SendAsync
         (new(req.ToByteArray()), WebSocketMessageType.Binary, true, token);
 
+    [Obsolete("Remove when handlers are okay")]
     private async Task<Response> ReceiveAsync()
     {
         var buffer = new ArraySegment<byte>(new byte[READ_BUFFER]);
@@ -74,6 +93,27 @@ internal class GameConnection
         Status = response.Status;
 
         return response;
+    }
+
+    private async Task ReceiveForever()
+    {
+        var buffer = new ArraySegment<byte>(new byte[READ_BUFFER]);
+        while (_socket.State == WebSocketState.Open)
+        {
+            WebSocketReceiveResult result;
+            using (var ms = new MemoryStream())
+            {
+                do
+                {
+                    result = await _socket.ReceiveAsync(buffer, CancellationToken.None);
+                    ms.Write(buffer.Array, buffer.Offset, result.Count);
+                } while (!result.EndOfMessage);
+
+                var msg = Response.Parser.ParseFrom(ms.GetBuffer(), 0, (int) ms.Position);
+                Status = msg.Status;
+                _responseHandler.Handle(msg.ResponseCase, msg);
+            }
+        }
     }
 
     public async Task Disconnect()
---
+++ SC2ClientApi/ResponseHandler.cs
@@ -0,0 +1,42 @@
+﻿using SC2APIProtocol;
+using ResponseType = SC2APIProtocol.Response.ResponseOneofCase;
+using RequestType = SC2APIProtocol.Request.RequestOneofCase;
+
+namespace SC2ClientApi;
+
+/// <summary>
+///     Handles responses from websockets sequentially by type
+///     ResponseOneofCase and RequestOneofCase are equal
+/// </summary>
+public class ResponseHandler
+{
+    private readonly IDictionary<ResponseType, Action<Response>> _handlers;
+
+    public ResponseHandler()
+    {
+        _handlers = new Dictionary<ResponseType, Action<Response>>();
+    }
+
+    public void RegisterHandler(RequestType key, Action<Response> handler) => RegisterHandler((ResponseType) key, handler);
+
+    public void RegisterHandler(ResponseType key, Action<Response> handler)
+    {
+        _handlers[key] = handler;
+    }
+
+    public void DeregisterHandler(RequestType key) => DeregisterHandler((ResponseType) key);
+
+    public void DeregisterHandler(ResponseType key)
+    {
+        _handlers.Remove(key);
+    }
+
+    public void Handle(RequestType key, Response response) => Handle((ResponseType) key, response);
+
+    public void Handle(ResponseType key, Response response)
+    {
+        // if (!_handlers.ContainsKey(key)) return;
+
+        _handlers[key](response);
+    }
+}
