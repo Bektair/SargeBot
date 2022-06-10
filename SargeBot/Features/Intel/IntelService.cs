2fabfb3d6be02b3359d4ab30eddfcfbdc648bd2b
Dagfinn Reitan <dagfinn.reitan@gyldendal.no>
2022-06-10 23:04:25 +0200

    Added enemy start location to IntelService

--- SargeBot/Features/Intel/IntelService.cs
+++ SargeBot/Features/Intel/IntelService.cs
@@ -10,14 +10,22 @@ public class IntelService
     public List<IntelColony> EnemyColonies { get; set; } = new();
     public List<Unit> Destructibles { get; set; } = new();
     public List<Unit> XelNagaTowers { get; set; } = new();
+    public List<Unit> SelfBuildings { get; set; } = new();
 
     public void OnStart(ResponseObservation firstObservation, ResponseData? responseData = null, ResponseGameInfo? gameInfo = null)
     {
+        if (gameInfo != null) 
+            EnemyColonies.Add(new() {Point = gameInfo.StartRaw.StartLocations.Last()});
+
         foreach (var unit in firstObservation.Observation.RawData.Units)
             switch (unit.Alliance)
             {
-                case Alliance.Self: // use gameInfo.StartRaw.StartLocations instead?
-                    if (unit.UnitType.IsMainBuilding()) SelfColonies.Add(new() {IsStartingLocation = true, Point = new() {X = unit.Pos.X, Y = unit.Pos.Y}});
+                case Alliance.Self:
+                    if (unit.UnitType.IsMainBuilding())
+                    {
+                        SelfColonies.Add(new() {IsStartingLocation = true, Point = new() {X = unit.Pos.X, Y = unit.Pos.Y}});
+                        SelfBuildings.Add(unit);
+                    }
                     break;
                 case Alliance.Neutral:
                     if (unit.UnitType.Is(UnitTypes.NEUTRAL_XELNAGATOWER)) XelNagaTowers.Add(unit);
